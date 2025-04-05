d21dc82817214a806f285925c481f60fa030de0d
Victor Massé <victor.dhamma@gmail.com>
2025-04-05 11:09:21 +0200

    up

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ function HomeContent() {
             <div className="flex flex-col space-y-2">
               <label
                 htmlFor="document"
-                className="text-sm font-medium leading-none">
+                className="text-sm font-medium leading-none sr-only">
                 {isLoading ? "Traitement en cours..." : ""}
               </label>
               <div className="flex items-center justify-center w-full">
@@ -103,7 +103,23 @@ function HomeContent() {
                   }`}>
                   <div className="flex flex-col items-center justify-center p-5">
                     {isLoading ? (
-                      <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900 my-4"></div>
+                      <svg
+                        className="animate-spin mr-3 h-5 w-5 text-gray-900 dark:text-gray-100"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24">
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      </svg>
                     ) : (
                       <>
                         <svg

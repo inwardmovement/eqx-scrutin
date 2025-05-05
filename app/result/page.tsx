23d8ccdd7ae34dbd6c3c2e85de2f00f513e6d0be
Victor Massé <victor.dhamma@gmail.com>
2025-05-05 09:27:35 +0200

    up

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -184,7 +184,7 @@ function ResultContent() {
     currentUrl.searchParams.set("d", "embed")
     const embedUrl = currentUrl.toString()
 
-    const embedCode = `<iframe class="iframeResize" style="border: none; width: 100%; height: 500px" src="${embedUrl}"></iframe>
+    const embedCode = `<iframe title="Résultat du scrutin" class="iframeResize" style="border: none; width: 100%; height: 500px" src="${embedUrl}"></iframe>
     <script>
       window.addEventListener("message", function (event) {
         if (event.data.type === "iframeHeight") {
